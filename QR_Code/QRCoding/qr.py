0797a0b06d6003883249c977fbcd4b929674003a
cavus <153572347+Halilcavus0@users.noreply.github.com>
2024-09-23 20:13:32 +0300

    Add files via upload

---
+++ QR_Code/QRCoding/qr.py
@@ -0,0 +1,17 @@
+import qrcode
+
+code = qrcode.QRCode(
+    version = 1,
+    box_size=100,
+    border=2
+)
+
+code.add_data("https://www.linkedin.com/in/halilcavus/")
+code.make(fit=True)
+
+image = code.make_image(fill_color="black",back_color="white")
+image.save("twice.png")
+
+
+
+print("QR Code oluşturuldu!.")
